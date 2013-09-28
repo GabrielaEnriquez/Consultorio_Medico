d1d0782a57eebaa5e0acd4d7597f6c2891062b9f
ramosgomez <ramosgomezgpe@gmail.com>
2013-09-27 23:58:10 -0500

    se agregaron las clases de doctor, diagnostico y cita

---
+++ ConsultorioMedico/src/org/ing/metodos/DoctorMet.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ing.metodos;
+
+/**
+ *
+ * @author Lupita
+ */
+public class DoctorMet {
+    public void RegistrarPaciente(){
+        
+    }
+    
+    public void RegistrarDiagnostico(){
+        
+    }
+    
+    public void ModificarDiagnostico(){
+        
+    }
+    
+    public void AgendarCita(){
+        
+    }
+    
+    public void CerrarSesion(){
+        
+    }
+    
+    public void EntradaAplicacion(){
+        
+    }
+    
+    public void GenerarCita(){
+        
+    }
+    
+    public void RegistrarTratamiento(){
+        
+    }
+    
+    public void VisualizarHistorial(){
+        
+    }
+    
+    public void ImprimirReceta(){
+        
+    }
+}
---
+++ ConsultorioMedico/src/org/ing/variables/Doctor.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ing.variables;
+
+/**
+ *
+ * @author Lupita
+ */
+public class Doctor {
+    private int cveD;
+    private String nombreD;
+    private String apellidosD;
+    private String direccionD;
+    private String especialidad;
+    private String curpD;
+    private String telefonoD;
+
+    public int getCveD() {
+        return cveD;
+    }
+
+    public void setCveD(int cveD) {
+        this.cveD = cveD;
+    }
+
+    public String getNombreD() {
+        return nombreD;
+    }
+
+    public void setNombreD(String nombreD) {
+        this.nombreD = nombreD;
+    }
+
+    public String getApellidosD() {
+        return apellidosD;
+    }
+
+    public void setApellidosD(String apellidosD) {
+        this.apellidosD = apellidosD;
+    }
+
+    public String getDireccionD() {
+        return direccionD;
+    }
+
+    public void setDireccionD(String direccionD) {
+        this.direccionD = direccionD;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getCurpD() {
+        return curpD;
+    }
+
+    public void setCurpD(String curpD) {
+        this.curpD = curpD;
+    }
+
+    public String getTelefonoD() {
+        return telefonoD;
+    }
+
+    public void setTelefonoD(String telefonoD) {
+        this.telefonoD = telefonoD;
+    }
+    
+    
+    
+}
